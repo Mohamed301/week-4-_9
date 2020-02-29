d80b79ed2fde1c24f0516babd65fff0cc9059440
Mohamed301 <49812336+Mohamed301@users.noreply.github.com>
2020-02-29 13:39:57 -0800

    Update server.R

--- git remote add origin https:/github.com/Mohamed301/server.R
+++ git remote add origin https:/github.com/Mohamed301/server.R
@@ -2,29 +2,6 @@
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data - Shiny Application and Reproducible Pitch-week4"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 100,
-                     value = 50)
-         
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
