e6ee3cb53cf780aac6463bb4a71d7b7f170327d1
Mohamed301 <49812336+Mohamed301@users.noreply.github.com>
2020-03-10 21:53:52 -0700

    Update UI.R

--- git remote add origin https:/github.com/Mohamed301/UI.R
+++ git remote add origin https:/github.com/Mohamed301/UI.R
@@ -1,25 +1,33 @@
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data - Shiny Application and Reproducible Pitch-week4"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 100,
-                     value = 50)
-         
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+shinyUI(fluidPage(
+      titlePanel("Prediction of height"),
+      sidebarLayout(
+            sidebarPanel(
+                  helpText("Prediction of the child's height considering gender and parent's height"),
+                  helpText("Parameters:"),
+                  sliderInput(inputId = "inFh",
+                              label = "Father's height (cm):",
+                              value = 150,
+                              min = 150,
+                              max = 220,
+                              step = 1),
+                  sliderInput(inputId = "inMh",
+                              label = "Mother's height (cm):",
+                              value = 140,
+                              min = 140,
+                              max = 200,
+                              step = 1),
+                  radioButtons(inputId = "inGen",
+                               label = "Child's gender: ",
+                               choices = c("Female"="female", "Male"="male"),
+                               inline = TRUE)
+            ),
+            
+            mainPanel(
+                  htmlOutput("pText"),
+                  htmlOutput("pred"),
+                  plotOutput("Plot", width = "50%")
+            )
       )
-   )
-)
+))
